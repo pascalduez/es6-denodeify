51d0b8edee4ed967fc658d4059f0fb993501c004
Pascal Duez <public@pascalduez.me>
2015-05-11 21:05:57 +0200

    Prevent global Promise constructor override

--- index.es6.js
+++ index.es6.js
@@ -1,5 +1,5 @@
-export default (Promise=Promise) => f =>
+export default (p = Promise) => f =>
   (...args) =>
-    new Promise((resolve, reject) =>
+    new p((resolve, reject) =>
       f(...args, (err, val) => err ? reject(err) : resolve(val))
     )
